584bf954498e5e71fc8b86f40086bf55793da3d1
ToniMaciel <jaam@cin.ufpe.br>
2021-03-19 21:11:09 -0300

    Adição de lock e condicional

--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class MusicPlayer {
 
-    static Lock lock = new ReentrantLock();
+    static Lock lock = new ReentrantLock(); //Lock que será usado na região critica (lista das músicas)
+    static Condition songslistOperation = lock.newCondition(); 
+    static boolean songsListInUse = false; //Para saber se alguma thread está fazendo alguma operação com a lista de reprodução
     static Scanner in = new Scanner(System.in);
     static ArrayList<Song> songsList = new ArrayList<>(); //Um array que armazenará todas as músicas até então adicionadas.
     
@@ -16,11 +19,14 @@ public class MusicPlayer {
     }
 
     /*
-    * Essa é a classe usada para adicionar uma música à lista de reprodução. 
-    * Ao executar essa thread, ela pedirá o nome da música, o nome do artista e a sua duração
-    * com essas informações será criado um novo objeto song que será adicionado ao array de músicas que representa a lista de reprodução.
+    * Essa é a classe usada para adicionar uma música à lista de reprodução.
+    * Com os parâmetros passados no seu construtor, faremos uma nova música 
+    * Ao executar essa thread, usaremos um lock para evitar uma condição de corrida
+    * Enquanto o boolean 'songsListInUse' significa que alguma thread está operando na região crítica
+    * e portanto, daremos um await no nosso condicional.
+    * Quando a outra thread terminar, esta thread de adição pode adicionar a música, enviar uma mensagem de sucesso e notificar as outras threads que terminou
     */
-    static class AddSongThread extends Thread {
+    static class AddSongThread extends Thread{
         Song newSong;
 
         public AddSongThread(String title, String singer, int duration) {
@@ -31,34 +37,65 @@ public class MusicPlayer {
             this.newSong.duration = duration;
         }
 
-        @Override public void run(){          
-            songsList.add(this.newSong);
+        @Override public void run(){
+            try {
+                lock.lock();
+                
+                while(songsListInUse){
+                    songslistOperation.await();
+                }
+                
+                songsListInUse = true;
 
-            System.out.println("Você adicionou a música: " + newSong.title);
+                songsList.add(this.newSong);
+                System.out.println("Você adicionou a música: " + newSong.title);
+                
+                songsListInUse = false;
+                songslistOperation.signalAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }          
         }
     }
 
     /*
     * Essa é a classe usada para imprimir a lista de reprodução. 
-    * Ao executar essa thread, ela percorrerá a lista de reprodução e imprimirá as características de cada uma das músicas contida nela.
+    * A lógica dessa thread para evitar a condição de corrida é a mesma comentada na thread de adição
+    * com exceção, claro, de que essa apenas lista as músicas que estão na lista de reprodução.
     */
     static class ListSongThread extends Thread {
         @Override public void run(){
-            System.out.println("Idx.\tMúsica\tArtista\tDuração");
-            for (int i = 0; i < songsList.size(); ++i){
-                Song s = songsList.get(i);
-                System.out.printf("%d.\t%s\t%s\t%d\n", i + 1, s.title.replaceAll(" ", ""), s.singer.replaceAll(" ", ""), s.duration);
+            try {
+                lock.lock();
+                
+                while(songsListInUse){
+                    songslistOperation.await();
+                }
+                                
+                songsListInUse = true;
+                
+                System.out.println("Idx.\tMúsica\tArtista\tDuração");
+                for (int i = 0; i < songsList.size(); ++i){
+                    Song s = songsList.get(i);
+                    System.out.printf("%d.\t%s\t%s\t%d\n", i + 1, s.title.replaceAll(" ", ""), s.singer.replaceAll(" ", ""), s.duration);
+                }
+                
+                songsListInUse = false;
+                songslistOperation.signalAll();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
             }
         }
     }
 
     /*
     * Essa é a classe usada para remover músicas da lista de reprodução. 
-    * Ao executar essa thread, ela checará se existem músicas na lista de reprodução
-    * Caso existam, será pedido a posição da música que deseja-se remover e em seguida
-    * as músicas contidas nessa lista são impressas sendo mostrado seu nome, seu artista e sua posição na lista
-    * Ao pegar o índice dado pelo usuário é observado se é um valor válido, caso não seja é printado uma mensagem dizendo que não foi possível executar o comando
-    * Caso seja, a música é removida e uma mensagem de sucesso é mostrada.
+    * A lógica dessa thread para evitar a condição de corrida é a mesma comentada na thread de adição
+    * com exceção, claro, de que essa removerá uma música de índice pedido da lista de reprodução.
     */
     static class RemoveSongThread extends Thread {
         int idx;
@@ -68,8 +105,26 @@ public class MusicPlayer {
         }
 
         @Override public void run(){
-            Song aux = songsList.remove(idx);
-            System.out.println("A música " + aux.title + " foi removida com sucesso!");
+
+            try {
+                lock.lock();
+                
+                while(songsListInUse){
+                    songslistOperation.await();
+                }
+                
+                songsListInUse = true;
+
+                Song aux = songsList.remove(idx);
+                System.out.println("A música " + aux.title + " foi removida com sucesso!");
+                
+                songsListInUse = false;
+                songslistOperation.signalAll();
+            }  catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            } 
             
         }
     }
@@ -83,7 +138,6 @@ public class MusicPlayer {
     * O comando 'help' serve para imprimir os comandos novamente
     * O comando 'ext' serve finalizar o sistema
     * Quaisquer outros comandos farão o programa emitir um aviso de comando inválido e buscarão um novo comando.
-    * As ordens de 'join' em cada um dos comandos servem para que a thread de interface só inicie outro comando quando finalizar o que estava executando 
     */
     static Thread UserInterfaceThread = new Thread() {
         @Override public void run(){
@@ -95,20 +149,20 @@ public class MusicPlayer {
                 input = in.nextLine(); //Pega cada novo comando
                 
                 if (input.equals("add")){
-                    System.out.println("Digite o nome da música:");
+                    System.out.println("Digite o nome da música:");  //Aqui será pedido para digitar as informações da música que deseja adicionar à lista
                     String title = in.nextLine();
                     System.out.println("Digite o nome do artista:");
                     String singer = in.nextLine();
                     System.out.println("Digite a duração em segundos:");
                     String duration = in.nextLine();
 
-                    AddSongThread addThread = new AddSongThread(title, singer, Integer.parseInt(duration));
+                    AddSongThread addThread = new AddSongThread(title, singer, Integer.parseInt(duration)); //As informações digitas serão usadas aqui, no construtor da thread
                     addThread.start();
                 } else if (input.equals("lst")){
                     ListSongThread listThread = new ListSongThread();
                     listThread.start();
                 } else if (input.equals("rmv")){
-                    if(songsList.isEmpty()){
+                    if(songsList.isEmpty()){  //Checa se a lista de reprodução não está vazia
                         System.out.println("Parece que você não tem músicas para remover :/");
                     } else {
                         System.out.println("Digite o índice da música que deseja remover:");
@@ -119,12 +173,12 @@ public class MusicPlayer {
                             i++;
                         }
                         
-                        int idx = in.nextInt(); in.nextLine();
+                        int idx = in.nextInt(); in.nextLine(); //Pega o índice da música que o usuário deseja remover
             
-                        if(idx < 1 || idx > songsList.size())
+                        if(idx < 1 || idx > songsList.size()) //Checa se é válido
                             System.out.println("Esse índice não existe :( Por favor use o comando 'rmv' novamente caso deseje excluir uma música");
                         else{
-                            RemoveSongThread removeThread = new RemoveSongThread(idx-1);
+                            RemoveSongThread removeThread = new RemoveSongThread(idx-1); //Usa o índice como parâmetro no construtor da thread
                             removeThread.start();
                         }
                     }
