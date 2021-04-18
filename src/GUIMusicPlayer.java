5494b72c2b5e5b7db6aea975e090b583c675686b
ToniMaciel <jaam@cin.ufpe.br>
2021-04-18 12:12:22 -0300

    Integração da GUI com o player de threads

---
+++ src/GUIMusicPlayer.java
@@ -0,0 +1,243 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.System;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.JScrollPane;
+import javax.swing.SwingWorker;
+import javax.swing.WindowConstants;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+public class GUIMusicPlayer extends MusicPlayer implements ActionListener, ListSelectionListener, WindowConstants {
+
+	//	idx guarda o valor do indice na lista da música que está em execução e progBarIdx guarda o valor que a musicProgressBar possui
+	int idx, progBarIdx = 0;
+	//	Variável auxiliar na definição de tempo de música
+	long songTime = 0;
+	//	Variável auxiliar de controle de botão pause/play (JBUtton stpMusicButton)
+	boolean paused = true;
+	
+
+	//	Inicializa os componentes do JavaSwing
+	private JButton addMusicButton, fwdMusicButton, stpMusicButton, bckMusicButton, rmvMusicButton;
+	private JLabel playingSong, currentTime, totalTime;
+	private JList musicTitlesList;
+	private JProgressBar musicProgressBar;
+	private JFrame frame;
+	private SwingWorker<Object, Object> progressBarUpdate = new SwingWorker<Object, Object>(){
+		@Override
+		protected Object doInBackground() throws Exception {
+			return null;
+		}
+	};
+
+	public GUIMusicPlayer() {
+
+		musicProgressBar = new JProgressBar();
+		musicProgressBar.setValue(0);
+		musicProgressBar.setBounds(20, 20, 420, 20);
+
+		currentTime = new JLabel("0:00");
+		currentTime.setBounds(20, 45, 40, 10);
+		totalTime = new JLabel("0:00");
+		totalTime.setBounds(410, 45, 40, 10);
+
+		playingSong = new JLabel("Now Playing: None");
+		playingSong.setBounds(20, 60, 420, 25);
+
+		bckMusicButton = new JButton("<<");
+		bckMusicButton.addActionListener(this);
+		bckMusicButton.setActionCommand("bck");
+		bckMusicButton.setBounds(20, 100, 60, 40);
+
+		stpMusicButton = new JButton("|>");
+		stpMusicButton.addActionListener(this);
+		stpMusicButton.setActionCommand("stp");
+		stpMusicButton.setBounds(85, 100, 50, 40);
+
+		fwdMusicButton = new JButton(">>");
+		fwdMusicButton.addActionListener(this);
+		fwdMusicButton.setActionCommand("fwd");
+		fwdMusicButton.setBounds(140, 100, 60, 40);
+
+		addMusicButton = new JButton("ADD");
+		addMusicButton.addActionListener(this);
+		addMusicButton.setActionCommand("add");
+		addMusicButton.setBounds(380, 100, 60, 40);
+
+		musicTitlesList = new JList();
+		musicTitlesList.setModel(songsList);
+		musicTitlesList.addListSelectionListener(this);
+		
+		JScrollPane scrollPane = new JScrollPane(musicTitlesList);
+		scrollPane.setViewportView(musicTitlesList);
+		scrollPane.setBounds(20, 150, 420, 110);
+
+		rmvMusicButton = new JButton("RMV");
+		rmvMusicButton.addActionListener(this);
+		rmvMusicButton.setActionCommand("rmv");
+		rmvMusicButton.setBounds(380, 270, 60, 40);
+
+		JPanel panel = new JPanel();
+		panel.setLayout(null);
+		panel.add(musicProgressBar);
+		panel.add(currentTime);
+		panel.add(totalTime);
+		panel.add(playingSong);
+		panel.add(bckMusicButton);
+		panel.add(stpMusicButton);
+		panel.add(fwdMusicButton);
+		panel.add(addMusicButton);
+		panel.add(scrollPane);
+		panel.add(rmvMusicButton);
+
+		frame = new JFrame();
+		frame.add(panel);
+		frame.setTitle("MusicPlayer");
+		frame.setSize(480, 360);
+		frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new GUIMusicPlayer();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+
+		switch (command) {
+			case "add":
+				String musicName     = JOptionPane.showInputDialog(frame, "Title", "Music Input", -1);
+				String musicArtist   = JOptionPane.showInputDialog(frame, "Artist", "Music Input", -1);
+				String musicDuration = JOptionPane.showInputDialog(frame, "Duration (in seconds)", "Music Input", -1);
+
+				addThread(musicName + " - " + musicArtist, Integer.parseInt(musicDuration));
+				
+				break;
+			case "rmv":
+				if(thereIsMusicSelected()){ //	Só executa se alguma música da lista estiver selecionada
+					removeThread(idx);
+
+					if (!progressBarUpdate.isDone())
+						progressBarUpdate.cancel(true);
+
+					try {
+						Thread.sleep(5);
+					} catch (InterruptedException e1) {
+						e1.printStackTrace();
+					}
+					
+					totalTime.setText("0:00");
+					currentTime.setText("0:00");
+					playingSong.setText("Now Playing: None");
+					stpMusicButton.setText("|>");
+					musicProgressBar.setValue(0);
+				}
+				break;
+			case "fwd": //	Executa próxima música de forma circular
+				if(thereIsMusicSelected()){
+					idx = (idx + 1) % songsList.size();
+					selectNewMusic();
+				}
+				break;
+			case "bck": // Executa música anterior de forma circular
+				if(thereIsMusicSelected()){
+					idx = (idx + songsList.size() - 1) % songsList.size();
+					selectNewMusic();
+				}
+				break;
+			case "stp":
+				paused = !paused;
+
+				if (paused){
+					stpMusicButton.setText("|>");
+					progressBarUpdate.cancel(true); //Cancela a thread que estava executando a musica selecionada no momento
+				}
+				else{
+					stpMusicButton.setText("||");
+					if (thereIsMusicSelected()) //Só dá paly em uma música, se houver alguma selecionada
+						callProgBar();
+				}
+				break;
+		}
+	}
+
+	@Override
+	public void valueChanged(ListSelectionEvent e) {
+
+		if(thereIsMusicSelected()){ //Verifica se o valor trocado é uma música da lista (Pode ser null caso remova a primeira música da lista, pois idx fica -1)
+			idx = musicTitlesList.getSelectedIndex();
+
+			startNewMusic();
+		}
+
+	}
+
+	private boolean thereIsMusicSelected() {
+		return musicTitlesList.getSelectedValue() != null;
+	}
+
+	private void selectNewMusic() {
+		musicTitlesList.setSelectedIndex(idx);
+	}
+
+	/**
+	 * 	Seta as variavéis necessárias para inicialização da música selecionada, como reinicialização do tempo de execução e da progressBar
+	 * 	bem como altera o botão de play/pause, a label que indica a música atual e o tempo total para sua execução.
+	 * 	Por último, chama a thread que vai executar a música.
+	 */
+	private void startNewMusic() {
+		
+		paused = false;
+		songTime = 0;
+		progBarIdx = 0;
+		musicProgressBar.setValue(0);
+		stpMusicButton.setText("||");
+		playingSong.setText("Now Playing: " + songsList.get(idx));
+		totalTime.setText(timeToString(duration.get(idx)));
+		callProgBar();
+			
+	}
+
+	//	Thread resposável por executar a música, alterando a progressBar e o tempo atual de execução da música
+	private void callProgBar() {
+
+		if (!progressBarUpdate.isDone())
+			progressBarUpdate.cancel(true);
+
+		progressBarUpdate = new SwingWorker<Object, Object>() {
+
+			@Override
+			protected Object doInBackground() throws Exception {
+				while (progBarIdx <= 100 && !isCancelled()) {
+					musicProgressBar.setValue(progBarIdx);
+
+					long now = System.currentTimeMillis();
+					while (System.currentTimeMillis() < now + 10 * duration.get(idx))
+						currentTime.setText(timeToString((int)(System.currentTimeMillis() + songTime - now)/1000));
+					
+					songTime += 10 * duration.get(idx);
+					progBarIdx++;
+				}
+
+				return 0;
+			}
+
+		};
+		progressBarUpdate.execute();
+	}
+
+	private String timeToString(int time){
+		return String.format("%d:%02d", time/60, time%60);
+	}
+
+}
--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,30 +1,17 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class MusicPlayer {
+import javax.swing.DefaultListModel;
 
-    static Lock lock = new ReentrantLock(); //Lock que será usado na região critica (lista das músicas)
-    static Condition songslistOperation = lock.newCondition(); 
-    static boolean songsListInUse = false; //Para saber se alguma thread está fazendo alguma operação com a lista de reprodução
-    static Scanner in = new Scanner(System.in);
-    static ArrayList<Song> songsList = new ArrayList<>(); //Um array que armazenará todas as músicas até então adicionadas.
+public class MusicPlayer {
     
-    //A classe das músicas. Considera que cada música terá um nome, um cantor e uma duração em segundos.
-    static class Song{
-        String title, singer;
-        int duration;
-        
-        public Song(String string, String string2, int i) {
-            this.title = string;
-            this.singer = string2;
-            this.duration = i;
-        }
-        public Song() {
-        }
-    }
+    public Lock lock = new ReentrantLock(); //Lock que será usado na região critica (lista das músicas)
+    public Condition songslistOperation = lock.newCondition();
+    public boolean songsListInUse = false; //Para saber se alguma thread está fazendo alguma operação com a lista de reprodução
+    public DefaultListModel<String> songsList = new DefaultListModel<>(); // Lista que possui strings que definem o nome da música e o artista
+    public ArrayList<Integer> duration = new ArrayList<>(); //	Lista que possui as durações das músicas de mesmo indice na lista anterior
 
     /*
     * Essa é a classe usada para adicionar uma música à lista de reprodução.
@@ -34,15 +21,13 @@ public class MusicPlayer {
     * e portanto, daremos um await no nosso condicional.
     * Quando a outra thread terminar, esta thread de adição pode adicionar a música, enviar uma mensagem de sucesso e notificar as outras threads que terminou
     */
-    static class AddSongThread extends Thread{
-        Song newSong;
+    class AddSongThread extends Thread{
+        String musicName;
+        int musicDuration;
 
-        public AddSongThread(String title, String singer, int duration) {
-            this.newSong = new Song();
-
-            this.newSong.title = title;
-            this.newSong.singer = singer;
-            this.newSong.duration = duration;
+        public AddSongThread(String title, int duration) {
+            this.musicName = title;
+            this.musicDuration = duration;
         }
 
         @Override public void run(){
@@ -55,8 +40,8 @@ public class MusicPlayer {
                 
                 songsListInUse = true;
 
-                songsList.add(this.newSong);
-                System.out.println("Você adicionou a música: " + newSong.title);
+                songsList.addElement(this.musicName);
+                duration.add(this.musicDuration);
                 
                 songsListInUse = false;
                 songslistOperation.signalAll();
@@ -73,7 +58,8 @@ public class MusicPlayer {
     * A lógica dessa thread para evitar a condição de corrida é a mesma comentada na thread de adição
     * com exceção, claro, de que essa apenas lista as músicas que estão na lista de reprodução.
     */
-    static class ListSongThread extends Thread {
+    public class ListSongThread extends Thread {
+
         @Override public void run(){
             try {
                 lock.lock();
@@ -83,12 +69,10 @@ public class MusicPlayer {
                 }
                                 
                 songsListInUse = true;
+
+                for (int i = 0; i < songsList.size(); ++i)
+                    System.out.println(songsList.get(i));
                 
-                System.out.println("Idx.\tMúsica\tArtista\tDuração");
-                for (int i = 0; i < songsList.size(); ++i){
-                    Song s = songsList.get(i);
-                    System.out.printf("%d.\t%s\t%s\t%d\n", i + 1, s.title.replaceAll(" ", ""), s.singer.replaceAll(" ", ""), s.duration);
-                }
                 
                 songsListInUse = false;
                 songslistOperation.signalAll();
@@ -98,6 +82,7 @@ public class MusicPlayer {
                 lock.unlock();
             }
         }
+
     }
 
     /*
@@ -105,11 +90,11 @@ public class MusicPlayer {
     * A lógica dessa thread para evitar a condição de corrida é a mesma comentada na thread de adição
     * com exceção, claro, de que essa removerá uma música de índice pedido da lista de reprodução.
     */
-    static class RemoveSongThread extends Thread {
+    class RemoveSongThread extends Thread {
         int idx;
 
-        public RemoveSongThread(int removeIndx){
-            this.idx = removeIndx;
+        public RemoveSongThread(int removeIdx){
+            this.idx = removeIdx;
         }
 
         @Override public void run(){
@@ -123,8 +108,8 @@ public class MusicPlayer {
                 
                 songsListInUse = true;
 
-                Song aux = songsList.remove(idx);
-                System.out.println("A música " + aux.title + " foi removida com sucesso!");
+                songsList.remove(this.idx);
+                duration.remove(this.idx);
                 
                 songsListInUse = false;
                 songslistOperation.signalAll();
@@ -137,82 +122,12 @@ public class MusicPlayer {
         }
     }
 
-    /*
-    * Essa é a thread usada para lidar com os comandos do usuário. 
-    * Assim que essa thread tem inicio, mostrará uma mensagem de boas vindas e os comandos que possui
-    * O comando 'add' inicia uma nova thread para adicionar uma música à lista
-    * O comando 'lst' inicia uma nova thread para listar as músicas da lista
-    * O comando 'rmv' inicia uma nova thread para remover uma música da lista
-    * O comando 'help' serve para imprimir os comandos novamente
-    * O comando 'ext' serve finalizar o sistema
-    * Quaisquer outros comandos farão o programa emitir um aviso de comando inválido e buscarão um novo comando.
-    */
-    public static void main(final String[] args) throws InterruptedException {
-        System.out.println("Bem-Vindo");
-        printCommands();
-        String input;
-        while (true){ //Esse laço se repetirá até que o usuário indique que deseja sair do programa
-            input = in.nextLine();
-            
-            switch (input) {
-                case "add":
-                    System.out.println("Digite o nome da música:"); 
-                    String title = in.nextLine();
-                    System.out.println("Digite o nome do artista:");
-                    String singer = in.nextLine();
-                    System.out.println("Digite a duração em segundos:");
-                    String duration = in.nextLine();
-                    AddSongThread addThread = new AddSongThread(title, singer, Integer.parseInt(duration));
-                    addThread.start();
-                    break;
-                
-                case "rmv":
-                    if(songsList.isEmpty()){  
-                        System.out.println("Parece que você não tem músicas para remover :/");
-                    } else {
-                        System.out.println("Digite o índice da música que deseja remover:");
-
-                        int i = 1; //Posição de cada música na lista de reprodução
-                        for (Song song : songsList) {
-                            System.out.println(i + ". " + song.title + " do artista " + song.singer);
-                            i++;
-                        }
-
-                        int idx = in.nextInt(); in.nextLine(); //Pega o índice da música que o usuário deseja remover
-                    
-                        if(idx < 1 || idx > songsList.size()) 
-                            System.out.println("Esse índice não existe :( Por favor use o comando 'rmv' novamente caso deseje excluir uma música");
-                        else{
-                            RemoveSongThread removeThread = new RemoveSongThread(idx-1);
-                            removeThread.start();
-                        }
-                    }
-                    break;              
-                
-                case "lst": 
-                    ListSongThread listThread = new ListSongThread();
-                    listThread.start();
-                    break;
-                
-                case "ext":
-                    System.exit(0);
-                    break;
-                
-                case "help":
-                    printCommands();
-                    break;
-
-                default:
-                    System.out.println("Comando inválido");
-                    break;
-            }
-        }
+    public void addThread(String title, int duration){
+        new AddSongThread(title, duration).start();
     }
 
-    //Imprime uma lista com cada um dos comandos e sua função
-    private static void printCommands() {
-        System.out.println("Digite o comando que você deseja:\n" + "add - Adicionar uma música\n" 
-        + "rmv - Remover uma música\n" + "lst - Listar as músicas que estão na lista\n" + 
-        "help - Mostrar comandos novamente\n" + "ext - Parar a execução");
+    public void removeThread(int index){
+        new RemoveSongThread(index).start();
     }
+
 }
